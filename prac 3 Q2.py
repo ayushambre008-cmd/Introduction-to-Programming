47c949cf684a3e89fc98be7e4025fd31b7a81dcf
ayushambre008-cmd <ayushambre008@gmail.com>
2026-07-22 10:52:03 +0530

    Add files via upload

---
+++ prac 3 Q2.py
@@ -0,0 +1,7 @@
+num = int(input("Enter your number: "))
+
+if num % 2 == 0:
+    print("The number is Even.")
+
+else:
+    print("The number is odd. ")
---
+++ pract 3 Q1.py
@@ -0,0 +1,4 @@
+num = int (input("Enter the number "))
+
+if num > 0:
+    print("The number is positive.")
